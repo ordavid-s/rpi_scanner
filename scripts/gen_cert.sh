c7bd91b74be8a0611094d627aa51608651e96196
ordavid-s <82959055+ordavid-s@users.noreply.github.com>
2026-03-29 09:58:32 +0300

    Update gen_cert.sh

--- scripts/gen_cert.sh
+++ scripts/gen_cert.sh
@@ -7,7 +7,7 @@ mkdir -p "$CERT_DIR"
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
   -keyout "$CERT_DIR/server.key" \
   -out "$CERT_DIR/server.crt" \
-  -subj "/CN=192.168.50.1"
+  -subj "/CN=192.168.7.1"
 
 chmod 600 "$CERT_DIR/server.key"
 chmod 644 "$CERT_DIR/server.crt"
